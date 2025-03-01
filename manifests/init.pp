bc8156a06e2a6f19f32b9b3a96b100e0e98b823e
Vishal Kharade <37978213+ceovishal@users.noreply.github.com>
2025-03-01 18:18:22 +0530

    Update init.pp

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,9 @@ class filecopy {
   if $facts['os']['family'] == 'RedHat' {
     file { '/home/rpm-4.20.1-1-omv2590.aarch64.rpm':
       ensure => 'present',
+      owner  => 'root',
+      group  => 'root',
+      mode   => '0644',
       source => 'puppet:///rpmandexes/rpm-4.20.1-1-omv2590.aarch64.rpm',
     }
   }
@@ -13,6 +16,9 @@ class filecopy {
   elsif $facts['os']['family'] == 'windows' {
     file { 'C:/temp/package.exe':
       ensure => 'present',
+      mode   => '0644',
+      owner  => 'Administrators',
+      group  => 'Administrators'
       source => 'puppet:///rpmandexes/puppet-agent-7.27.0-x86.msi',
     }
   }
